05fcc97214c85014d6f7e5b307ee6580c10cf55a
s160421001 <s160421001@student.ubaya.ac.id>
2023-01-06 11:51:52 +0700

    .

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -6,49 +6,67 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
-    public float speed;
-    public float turnSpeed;
+    public Rigidbody theRB;
 
-    private Rigidbody rb;
+    public float maxSpeed;
+
+    public float forwardAccel = 8f, reverseAccel = 4f;
+    private float speedInput;
+
+    public float turnStrength = 180f;
+    private float turnInput;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
         
     }
     private void Update()
     {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel;
+        }
 
+        turnInput = Input.GetAxis("Horizontal");
     }
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            //rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed * 10);
-            rb.velocity = speed * transform.forward;
-            //Vector3 velo = rb.velocity * speed;
-            //rb.AddForce(velo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            //rb.AddRelativeForce(-(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed * 10) / 2);
-            rb.velocity = speed * -transform.forward;
-        }
+        theRB.AddForce(transform.forward * speedInput * 1000f);
 
-        Vector3 rot = new Vector3(0f, turnSpeed, 0f);
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            Quaternion angle = Quaternion.Euler(rot);
-            rb.MoveRotation(angle * rb.rotation);
-            //rb.AddTorque(Vector3.up * turnSpeed * 10);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Quaternion angle = Quaternion.Euler(-rot);
-            rb.MoveRotation(angle * rb.rotation);
-            //rb.AddTorque(-Vector3.up * turnSpeed * 10);
-        }
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    //rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed * 10);
+        //    rb.velocity = speed * transform.forward;
+        //    //Vector3 velo = rb.velocity * speed;
+        //    //rb.AddForce(velo, ForceMode.VelocityChange);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    //rb.AddRelativeForce(-(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed * 10) / 2);
+        //    rb.velocity = speed * -transform.forward;
+        //}
+
+        //Vector3 rot = new Vector3(0f, turnSpeed, 0f);
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Quaternion angle = Quaternion.Euler(rot);
+        //    rb.MoveRotation(angle * rb.rotation);
+        //    //rb.AddTorque(Vector3.up * turnSpeed * 10);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    Quaternion angle = Quaternion.Euler(-rot);
+        //    rb.MoveRotation(angle * rb.rotation);
+        //    //rb.AddTorque(-Vector3.up * turnSpeed * 10);
+        //}
     }
     
 }
